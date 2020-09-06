f08e13dd9d2acc40d0f6ec640a92a4b0061a6139
curecox <nikola_998@yahoo.com>
2020-09-06 15:08:27 +0200

    Project added. Model added.

---
+++ RES/Common/DataModel/Measurement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.DataModel
+{
+    public class Measurement
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Component { get; set; }
+        public double Value { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public Measurement()
+        {
+            
+        }
+
+        public Measurement(string component, double value)
+        {
+            Component = component;
+            Value = value;
+            TimeStamp = DateTime.Now;
+        }
+    }
+}
