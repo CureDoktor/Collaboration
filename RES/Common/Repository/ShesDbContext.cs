f8c3d418e9be1e9f7d1227bf51e7fea14d4d2769
bogatinovic.v <bogatinovic.v@gmail.com>
2020-09-11 14:23:32 +0200

    Repository added.

---
+++ RES/Common/Repository/ShesDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Common.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Repository
+{
+    public class ShesDbContext : DbContext
+    {
+        public DbSet<SolarPanel> SolarPanels { get; set; }
+        public DbSet<Battery> Batteries { get; set; }
+        public DbSet<Consumer> Consumers { get; set; }
+        public DbSet<Utility> Utility { get; set; }
+        public DbSet<Measurement> Measurements { get; set; }
+
+        public ShesDbContext() : base("shes")
+        {
+
+        }
+    }
+}
---
+++ RES/Common/Repository/WriteMeasurementToDb.cs
@@ -0,0 +1,30 @@
+﻿using Common.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Repository
+{
+    public class WriteMeasurementToDb : IWriteMeasurementToDb
+    {
+        public void WriteMeasurement(string componentId, double value)
+        {
+            Measurement m = new Measurement(componentId, value);
+
+            try
+            {
+                using (ShesDbContext context = new ShesDbContext())
+                {
+                    context.Measurements.Add(m);
+                    context.SaveChanges();
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error while writing measurement to db. " + e.Message);
+            }
+        }
+    }
+}
